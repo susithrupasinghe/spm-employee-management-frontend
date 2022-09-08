9f68f4bac4d9f26b071b4fa82379d24a02b1c9a1
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-09-08 17:20:21 +0530

    Define routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Landing from './common/header/headerComponent';
+
 import './App.css';
+import ProjectManagement from "./pages/ProjectManager/ProjectDashboard/projectDashboard";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <Landing/>
+      <Routes>
+      <Route excat path="/pm/projectmanagement" element={<ProjectManagement />} />
+      </Routes>
+    </Router>
   );
 }
 
