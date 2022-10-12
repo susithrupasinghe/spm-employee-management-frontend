96254fd9aa13301cf57dcd21653e7746cc3f4692
Shavidini Ekanayake <77123486+ShavidiniEkanayake@users.noreply.github.com>
2022-10-13 03:26:31 +0530

    Completed project overview part

--- src/pages/EmployeeManagement/ProjectDocumentation/ViewDocumentation.js
+++ src/pages/EmployeeManagement/ProjectDocumentation/ViewDocumentation.js
@@ -1,62 +1,115 @@
 import { useEffect, useState } from "react";
+import { Input, AutoComplete } from "antd";
 import axios from "axios";
 
+// function getRandomInt(max, min = 0) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min; // eslint-disable-line no-mixed-operators
+// }
+
+// const searchResult = (query) =>
+//   new Array(getRandomInt(5))
+//     .join(".")
+//     .split(".")
+//     .map((item, idx) => {
+//       const category = `${query}${idx}`;
+//       return {
+//         value: category,
+//         label: (
+//           <div
+//             style={{
+//               display: "flex",
+//               justifyContent: "space-between",
+//             }}
+//           >
+//             <span>
+//               Found {query} on{" "}
+//               <a
+//                 href={`https://s.taobao.com/search?q=${query}`}
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//               >
+//                 {category}
+//               </a>
+//             </span>
+//             <span>{getRandomInt(200, 100)} results</span>
+//           </div>
+//         ),
+//       };
+//     });
+
 const ViewEmployeeForm = () => {
-    const [table, setTable] = useState("");
-  
-    return (
-      <div class="row" style={{ marginLeft: "20rem" }}>
-        <div class="col-sm-8">
-          <div class="card" style={{ borderRadius: "15px", marginTop: "5rem" }}>
-            <div class="card-body">
-              <h5 class="card-title" style={{ marginLeft: "13rem", fontFamily:"Inter" }}>
-              Project Documentation
-              </h5>
-              <form  style={{ marginTop: "1.5rem" }}>
-                <input
-                  type="text"
-                  name="documentName"
-                  style={{ marginBottom: "1rem" }}
-                  class="form-control"
-                  placeholder="Document Title"
-                />
-                <textarea
-                  type="textarea"
-                  name="email"
-                  style={{ marginBottom: "1rem" }}
-                  class="form-control"
-                  placeholder="Document Description"
-                />
-                <button
-                  type="submit"
-                  class="btn btn-dark"
-                  style={{ padding: "5px 40px" }}
-                >
-                  ADD
-                </button>
-              </form>
-            </div>
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(false);
+  // const [data, setData] = useState();
+  // // const [table, setTable] = useState("");
+
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        `http://localhost:5000/api/documentation/readDocmentationByProject`
+      );
+      // setIssueList(result.data);
+      console.log(result.data);
+      // setIsLoaded(true);
+    };
+    fetchData();
+  }, []);
+
+  // const handleSearch = (value) => {
+  //   setOptions(value ? searchResult(value) : []);
+  // };
+
+  // const onSelect = (value) => {
+  //   console.log("onSelect", value);
+  // };
+
+  return (
+    <div class="row" style={{}}>
+      <div class="col-sm-4">
+        <h3
+          class="card-title"
+          style={{ marginLeft: "1rem", marginTop: "5rem" }}
+        >
+          Project Documentation
+        </h3>
+        <div class="card" style={{ borderRadius: "15px", marginTop: "5rem" }}>
+          <div class="card-body">
+            <ul>
+              
+            </ul>
+            {/* <AutoComplete
+              dropdownMatchSelectWidth={252}
+              style={{
+                width: 300,
+              }}
+              options={options}
+              onSelect={onSelect}
+              onSearch={handleSearch}
+            >
+              <Input.Search size="large" placeholder="input here" enterButton />
+            </AutoComplete> */}
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 const ViewDocumentation = () => {
-    return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <ViewEmployeeForm />
-            </div>
-            <div className="col-lg-12">
-              {/* <Header /> */}
-            </div>
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <ViewEmployeeForm />
           </div>
+          <div className="col-lg-12">{/* <Header /> */}</div>
         </div>
       </div>
-    );
-  };
-  
-  export default ViewDocumentation;
+    </div>
+  );
+};
+
+export default ViewDocumentation;
