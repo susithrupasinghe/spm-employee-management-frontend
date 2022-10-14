37b616bf35c0badeb160ac9ea14ac1bf18bd454e
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-10-14 10:33:26 +0530

    Update employee page

--- src/pages/ProjectManager/RecruitEmployee/RecruitEmployee.js
+++ src/pages/ProjectManager/RecruitEmployee/RecruitEmployee.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -19,29 +20,68 @@ const Employees = () => {
   const [projectId, setProjectId ] = useState("");
   const [title, setTitle] = useState("");
   const [employeeList, setEmployeeList] = useState([]);
+  const marketingEmployees = employeeList.filter((employee) => employee.department === "marketing");
+  const developmentEmployees = employeeList.filter((employee) => employee.department === "development");
+  const testingEmployees = employeeList.filter((employee) => employee.department === "testing");
   useEffect(() => {
     const fetchData = async () => {
       setProjectId(location.state.projectId);
       setTitle(location.state.title);
       setEmployeeList(location.state.employeeList);
+      axios.get("")
     }
     fetchData();
   },[]);
+  console.log(employeeList);
 
   return(
+    <>
     <div className="mt-3 mx-5 px-2">
       <div className="card boderRadiusCards px-5">
         <div className="card-body">
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
-                {title}
+                <h3>Marketing</h3>
+                <div className="flex">
+                <span>employee</span>
+                  {marketingEmployees.map((employee) => (
+                    <span>{employee}</span>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <div className="mt-3 mx-5 px-2">
+      <div className="card boderRadiusCards px-5">
+        <div className="card-body">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+              <h3>Development</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="mt-3 mx-5 px-2">
+      <div className="card boderRadiusCards px-5">
+        <div className="card-body">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+              <h3>Testing</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 
 }
