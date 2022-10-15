9b14fbbe81ab562c63226976a08ca5ba16985b91
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-10-15 12:36:38 +0530

    Implement delete function

--- src/pages/Admin/AdminProjectManagement/AdminProjectManagement.js
+++ src/pages/Admin/AdminProjectManagement/AdminProjectManagement.js
@@ -42,6 +42,18 @@ const ModelPopUp = (props) => {
     } catch (error) {}
   };
 
+  const handleDelete = (id) => {
+    if(window.confirm("Are you sure to delete this record?")) {
+      axios.delete(`http://localhost:5000/api/project/${id}`)
+      .then((res) => {
+        if(res) {
+          alert("Record deleted successfully");
+          window.location.reload();
+        }
+      })
+    }
+  }
+
   return (
     <tr>
       <td>{props.project._id}</td>
@@ -52,7 +64,7 @@ const ModelPopUp = (props) => {
           style={{ color: "#A80038", marginRight: "20px" }}
           onClick={toggle}
         />
-        <MdDelete size={25} style={{ color: "#A80038" }} />
+        <MdDelete size={25} style={{ color: "#A80038" }} onClick={() =>handleDelete(props.project._id)} />
       </td>
 
       <Modal isOpen={modal} toggle={toggle}>
